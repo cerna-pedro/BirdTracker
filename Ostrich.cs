eb6b6cd2b0cf10fe2e898f2e0b6844c47ab3a04c
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-09-03 07:19:19 -0500

    Added Ostrich class

--- Ostrich.cs
+++ Ostrich.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace BirdTracker
 {
-    internal class Pigeon :Bird
+    internal class Ostrich: Bird
     {
-        public const double MAXIMUM = 3.0;
-        public const double MINIMUM = 1.0;
+        public const double MAXIMUM = 13.0;
+        public const double MINIMUM = 12.0;
         public override Egg[] LayEggs(int numberOfEggs)
         {
             Egg[] eggs = new Egg[numberOfEggs];
             for (int i = 0; i < numberOfEggs; i++)
             {
-                eggs[i] = new Egg(Randomizer.NextDouble()*(MAXIMUM-MINIMUM)+MINIMUM, "white");
+                eggs[i] = new Egg(Randomizer.NextDouble() * (MAXIMUM-MINIMUM) + MINIMUM, "speckled");
             }
             return eggs;
         }
