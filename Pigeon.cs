7255b492e4495f5212cbf210374424142d495aed
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-09-03 07:10:25 -0500

    Added Pigeon class

---
+++ Pigeon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdTracker
+{
+    internal class Pigeon :Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] eggs = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Randomizer.NextDouble()*(3.0-1.0)+1.0, "white");
+            }
+            return eggs;
+        }
+    }
+}
